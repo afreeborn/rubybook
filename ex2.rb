4b66118b0c468f41932d9dc8f1bdc7b12e007725
afreeborn <andrew242@hotmail.com>
2016-12-28 17:52:14 -0600

    Adding exercise 2 Ruby script

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# a comment
+# anything after the # is ignored
+
+puts "I could have code like this." # comments here
+
+# You can also use a comment to "disable" code
+# puts "This won't run."
+
+puts "This will run."
