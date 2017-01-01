23859070ec6ce132cb925db8966ececadd00b6eb
afreeborn <andrew242@hotmail.com>
2017-01-01 14:39:03 -0600

    Adding ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Andrew Freeborn'
+my_age = 99 # changed from the text
+my_height = 5 # inches???
+my_weight = 10 # lbs???
+my_eyes = 'My Color'
+my_teeth = 'White'
+my_hair = 'This One'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "He's got #{my_eyes} and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and my #{my_weight} weight, I get #{my_age + my_height + my_weight}."
